3800d44212eb348baa630f5fb27d8ef243ba8ac8
Tolugold1 <toluadelekesteve@gmail.com>
2022-08-04 10:02:12 +0000

    console update

--- console.py
+++ console.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """program that contains the entry point of the command interpreter"""
 import cmd
+import shlex
 import sys
 from models.base_model import BaseModel
 from models import storage
@@ -83,6 +84,38 @@ class HBNBCommand(cmd.Cmd):
                     print(dic_str_list)
             else:
                 print("** class doesn't exist **")
+
+    def do_update(self, arg):
+        """Update an instance based on the class name and id"""
+        if not arg:
+            print("** class name missing **")
+            return
+        new_arg = shlex.split(arg)
+        if new_arg[0] not in self.list_of_class:
+            print("** class doesn't exist **")
+        if not new_arg[1]:
+            print("** instance id missing **")
+        else:
+            try:
+                j = new_arg[0] + "." + new_arg[1]
+                storage.all()[j]
+            except:
+                print("** no instance found **")
+                return
+        if not new_arg[2]:
+            print("** attribute name missing **")
+        if not new_arg[3]:
+            print("** value missing **")
+        else:
+            j = new_arg[0] + "." + new_arg[1]
+            if new_arg[3] is float:
+                value = float(new_arg[3]) # casting value unto it datatype
+            elif new_arg[3] is int:
+                value = int(new_arg[3]) # int casting
+            else:
+                value = str(new_arg[3].strip(":\"'"))
+            setattr(storage.all()[j], new_arg[2].strip(":\"'"), value) # test for dictionary attribute
+            storage.save()
 
     def emptyline(self):
         """Do nothing if no command is given"""
