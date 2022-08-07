c62f0e3f28aec987978131f6118a6a00eb61260b
Tolugold1 <toluadelekesteve@gmail.com>
2022-08-07 14:07:25 +0000

    update

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -3,11 +3,33 @@
 import unittest
 from models.base_model import BaseModel
 import uuid
-from datetime import datetime
+from datetime import datetime, time, date
+import os
+import pep8
 
 
 class TestBaseModel(unittest.TestCase):
     """Test cases for class BaseModel"""
+
+    def setUp(self):
+        """displace json file from it's position if it exits"""
+        if os.path.isfile("file.json"):
+            os.rename("file.json", "file.json.temp")
+
+    def tearDown(self):
+        """delete json file and return the original tests file"""
+        if os.path.isfile("file.json"):
+            os.remove("file.json")
+        if os.path.isfile("file.json.temp"):
+            os.rename("file.json.temp", "file.json")
+
+    def test_pep8(self):
+        """Applying pep8 checker to my baseModel class"""
+        style = pep8.StyleGuide()
+        result = style.check_files(["models/base_model.py"])
+        self.assertEqual(result.total_errors, 0,
+                         "there's an error found in the model")
+        
     def test_attribute(self):
         """test if a class has Id, created_at and updated_at"""
         sample = BaseModel()
